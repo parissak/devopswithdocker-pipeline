0b26e978d4bf7ae223bb932788a4f0585d16766b
parissak <sapak@protonmail.com>
2024-05-14 22:32:36 +0300

    make change for testing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.send('<h1 style="color:blue">Hello!</h1>')
 })
 
 const PORT = 8080
